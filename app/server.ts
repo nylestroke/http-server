c23971903684a6a964df42bb0edc6e5421d15db7
Vladyslav Potapenko <nylestroke@gmail.com>
2022-12-01 13:01:57 +0100

    Update server.ts

--- app/server.ts
+++ app/server.ts
@@ -1,18 +1,13 @@
-import * as https from "https";
+import * as http from "http";
 import * as fs from "fs";
 import TodoController from "./controllers/TodoController";
 
 const PORT = process.env.PORT || 5000;
 
-const options = {
-    key: fs.readFileSync('./app/fixtures/keys/agent2-key.pem'),
-    cert: fs.readFileSync('./app/fixtures/keys/agent2-cert.cert')
-};
-
-const server = https.createServer(options, async (req, res) => {
+const server = http.createServer(async (req, res) => {
     await new TodoController().setEndpoints(req, res);
 });
 
 server.listen(PORT, () => {
-    console.log(`Server started on: https://localhost:${PORT}`);
+    console.log(`Server started on: http://localhost:${PORT}`);
 });
